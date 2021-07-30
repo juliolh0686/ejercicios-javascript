52097e61e809edb4514fdeb22f6368935da5b0b6
Julio <juliolh06@gmail.com>
2021-07-30 16:34:10 -0500

    Update

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -9,4 +9,29 @@ navToggle.addEventListener("click",()=>{
    }else{
        navToggle.setAttribute("aria-label","Abrir Menú");
    }
-})
+});
+
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    //Guardamos el modo en localstorage
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode','true');
+    }else{
+        localStorage.setItem('dark-mode','false');
+    }
+});
+
+//obtenemos el modo actual
+
+if(localStorage.getItem('dark-mode')==='true'){
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+} else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
